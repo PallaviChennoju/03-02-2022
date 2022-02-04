ab1de41ee67af12f7fa16dccffb8e030de4b75ad
admin <admin@DESKTOP-L61T1KU>
2022-02-04 12:33:16 +0530

    first commit

---
+++ 03-04-2022/src/com/javaDataTypes/Example2.java
@@ -0,0 +1,17 @@
+package com.javaDataTypes;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		/**
+		 * write a java program to print customer information (id, name and address)
+		 */
+      int customerid = 103;
+      String customerName = "Niharika";
+      String customerAddress = "Hyderabad";
+      System.out.println(customerid);
+      System.out.println(customerName);
+      System.out.println(customerAddress);
+	}
+
+}
